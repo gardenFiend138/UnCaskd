9d521c4d89bdbb95c956c5f4c7914b2b965942f6
Guy Wassather <ggwass@gmail.com>
2017-11-22 07:43:43 -0800

    Update todo list for today

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  helper_method :current_user, :signed_in?
+  helper_method :current_user, :logged_in?
 
   private
   # Neat ruby metaprogramming going on under the hood with method_missing
