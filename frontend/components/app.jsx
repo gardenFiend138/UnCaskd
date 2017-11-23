6efcad9b62df1da84d2e7b042aeb65e477932237
Guy Wassather <ggwass@gmail.com>
2017-11-22 21:30:19 -0800

    Render navbar

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -10,15 +10,21 @@ import {
 
 import SessionFormContainer from './session_form/session_form_container';
 import SplashPageContainer from './splash_page/splash_page_container';
+import Navbar from './navbar/navbar_container';
+import { AuthRoute, ProtectedRoute } from '../util/route_util';
+
 const App = () => (
   <div>
     <header>
-      <h1>Welcome to UnCaskd!</h1>
+      <Link to="/" className="header-link">
+        <h1>Welcome to UnCaskd!</h1>
+      </Link>
+      <Navbar />
     </header>
     <Switch>
-      <Route path='/login' component={SessionFormContainer} />
-      <Route path='/signup' component={SessionFormContainer} />
-      <Route path='/users/show' component={SplashPageContainer} />
+      <AuthRoute path='/login' component={SessionFormContainer} />
+      <AuthRoute path='/signup' component={SessionFormContainer} />
+      <Route path='/home' component={SplashPageContainer} />
     </Switch>
   </div>
 );
