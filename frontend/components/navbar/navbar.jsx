9c7cde92e25b96c7c51a6669b904796a16560ee5
Guy Wassather <ggwass@gmail.com>
2017-11-26 00:34:26 -0800

    Finish navbar layout

--- frontend/components/navbar/navbar.jsx
+++ frontend/components/navbar/navbar.jsx
@@ -1,21 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const navbarLinks = () => {
+const navbarLinks = ({ currentUser, logout }) => {
   return(
     <header className='navbar'>
       <ul className='nav-left'>
-        <li><div className='nav-logo'>
-          <h2>UnCaskd</h2>
-          <h5>Discover the Best</h5>
-        </div></li>
+        <li className='nav-logo'>
+          <Link to='/home'>
+            <h3>UnCaskd</h3>
+            <h5>Discover the Best</h5>
+          </Link>
+        </li>
         <li><Link to='/lounge'>The Lounge</Link></li>
         <li><Link to='/whiskies'>Top Rated</Link></li>
       </ul>
-      <ul className='nav-user'>
-        <li>Profile</li>
-        <li>Friends</li>
-        <li><Link to={}</li>
+
+      <ul className='nav-right'>
+        <li className='user-greeting'>Welcome back, {currentUser.username}!</li>
+        <li><img className='profile-pic' src='/assets/barrel-52934_1280.jpg' alt="photo"></img></li>
+        <li><input type='text' placeholder='Search...' /></li>
+
       </ul>
     </header>
   );
