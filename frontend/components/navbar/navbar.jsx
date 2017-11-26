7e6cd781f35564a0dc3dcf6e74667d7b8c64908d
Guy Wassather <ggwass@gmail.com>
2017-11-26 00:59:34 -0800

    Start on dropdown menu

--- frontend/components/navbar/navbar.jsx
+++ frontend/components/navbar/navbar.jsx
@@ -1,26 +1,38 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const navbarLinks = ({ currentUser, logout }) => {
+const dropDown = ({ currentUser, logout }) => {
+  return(
+    <div className='dropdown-menu'><ul>
+      <li>Profile</li>
+      <li>Friends</li>
+      <li><Link onClick={() => logout()}>Sign Out</Link></li>
+    </ul></div>
+  );
+};
+
+const navbarLinks = ({ currentUser }) => {
   return(
     <header className='navbar'>
-      <ul className='nav-left'>
-        <li className='nav-logo'>
-          <Link to='/home'>
-            <h3>UnCaskd</h3>
-            <h5>Discover the Best</h5>
-          </Link>
-        </li>
-        <li><Link to='/lounge'>The Lounge</Link></li>
-        <li><Link to='/whiskies'>Top Rated</Link></li>
-      </ul>
 
-      <ul className='nav-right'>
-        <li className='user-greeting'>Welcome back, {currentUser.username}!</li>
-        <li><img className='profile-pic' src='/assets/barrel-52934_1280.jpg' alt="photo"></img></li>
-        <li><input type='text' placeholder='Search...' /></li>
+        <ul className='nav-left'>
+          <li className='nav-logo'>
+            <Link to='/home'>
+              <h3>UnCaskd</h3>
+              <h5>Discover the Best</h5>
+            </Link>
+          </li>
+          <li><Link to='/lounge'>The Lounge</Link></li>
+          <li><Link to='/whiskies'>Top Rated</Link></li>
+        </ul>
 
-      </ul>
+        <ul className='nav-right'>
+          <li className='user-greeting'>Welcome back, {currentUser.username}!</li>
+          <li><img className='profile-pic' src='/assets/barrel-52934_1280.jpg' alt="photo"></img></li>
+          <li><input type='text' placeholder='Search...' /></li>
+
+        </ul>
+
     </header>
   );
 };
