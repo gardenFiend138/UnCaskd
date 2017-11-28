4f5a82ce86e3025c3e93676f5515d150a189ab64
Guy Wassather <ggwass@gmail.com>
2017-11-27 21:32:06 -0800

    Finish checkin actions

---
+++ frontend/actions/checkin_actions.js
@@ -0,0 +1,66 @@
+import * as APIUtil from '../util/checkin_api_util';
+
+export const RECEIVE_CHECKIN = 'RECEIVE_CHECKIN';
+export const RECEIVE_ALL_CHECKINS = 'RECEIVE_ALL_CHECKINS';
+export const RECEIVE_CHECKIN_ERRORS = 'RECEIVE_CHECKIN_ERRORS';
+export const REMOVE_CHECKIN = 'REMOVE_CHECKIN';
+
+export const receiveCheckin = checkin => ({
+  type: RECEIVE_CHECKIN,
+  checkin
+});
+
+export const receiveAllCheckins = checkins => ({
+  type: RECEIVE_ALL_CHECKINS,
+  checkins
+});
+
+export const receiveCheckinErrors = errors => ({
+  type: RECEIVE_CHECKIN_ERRORS,
+  errors
+});
+
+export const removeCheckin = checkinId => ({
+  type: REMOVE_CHECKIN,
+  checkinId
+});
+
+export const createCheckin = checkin => dispatch => (
+  APIUtil.createCheckin(checkin).then(checkin => (
+    dispatch(receiveCheckin(checkin))
+  ), err => (
+    dispatch(receiveCheckinErrors(err.responseJSON))
+  ))
+);
+
+export const updateCheckin = checkin => dispatch => (
+  APIUtil.updateCheckin(checkin).then(checkin => (
+    dispatch(receiveCheckin(checkin))
+  ), err => (
+    dispatch(receiveCheckinErrors(err.responseJSON))
+  ))
+);
+
+export const fetchCheckin = checkinId => dispatch => (
+  APIUtil.fetchCheckin(checkinId).then(checkin => (
+    dispatch(receiveCheckin(checkin))
+  ), err => (
+    dispatch(receiveCheckinErrors(err.responseJSON))
+  ))
+);
+
+export const fetchCheckins = () => dispatch => (
+  APIUtil.fetchCheckins().then(checkin => (
+    dispatch(receiveAllCheckins(checkin))
+  ), err => (
+    dispatch(receiveCheckinErrors(err.responseJSON))
+  ))
+);
+
+export const deleteCheckin = checkinId => dispatch => (
+  APIUtil.deleteCheckin(checkinId).then(checkin => (
+    dispatch(removeCheckin(checkin))
+  ), err => (
+    dispatch(receiveCheckinErrors(err.responseJSON))
+  ))
+);
---
+++ frontend/util/checkin_api_util.jsx
@@ -0,0 +1,36 @@
+export const createCheckin = checkin => (
+  $.ajax({
+    method: 'POST',
+    url: '/api/checkins',
+    data: { checkin }
+  })
+);
+
+export const updateCheckin = checkin => (
+  $.ajax({
+    method: 'PATCH',
+    url: `/api/checkins/${checkin.id}`,
+    data: { checkin }
+  })
+);
+
+export const fetchCheckins = () => (
+  $.ajax({
+    method: 'GET',
+    url: '/api/checkins'
+  })
+);
+
+export const fetchCheckin = checkinId => (
+  $.ajax({
+    method: 'GET',
+    url: `/api/checkins/${checkinId}`
+  })
+);
+
+export const deleteCheckin = checkinId => (
+  $.ajax({
+    method: 'DELETE',
+    url: `/api/checkins/${checkinId}`
+  })
+);
