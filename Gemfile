625e84414cb12a6af9302d28552f87a212a4f28e
Guy Wassather <ggwass@gmail.com>
2017-12-01 16:07:54 -0800

    Get webpack, package.json, and gemfile production ready

--- Gemfile
+++ Gemfile
@@ -33,9 +33,9 @@ gem 'faker'
 gem 'font-awesome-rails'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
-group :production do
-  gem 'rails_12factor'
-end
+
+gem 'rails_12factor'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,17 +1,19 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-import logger from 'redux-logger';
-// import { composeWithDevTools } from 'redux';
-
 import rootReducer from '../reducers/root_reducer';
 
+const middlewares = [thunk];
+
+if (process.env.NODE_ENV !== 'production') {
+  const { logger } = require('redux-logger');
+  middlewares.push(logger);
+}
 const configureStore = (preloadedState = {}) => {
 
   return createStore(
     rootReducer,
     preloadedState,
-    applyMiddleware(thunk, logger)
-    // composeWithDevTools(applyMiddleware(thunk, logger))
+    applyMiddleware(...middlewares)
   );
 };
 
